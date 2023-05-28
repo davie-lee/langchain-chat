60bf0b57ec36587ba6f6c8bd67a6813cffe87136
Davie Lee <daviehslee@gmail.com>
2023-05-28 17:11:51 -0400

    Removed OpenAI key - oops

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-from langchain import LanguageModel
-import openai
+def main():
+    print("Hello World!")
 
-# Get your OpenAI API key from https://openai.com/account
-openai.api_key = "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"
 
-# Load the langchain model
-model = LanguageModel("openai")
-
-# Read the PDF file
-pdf = open("MD-CA-AB-00001.pdf", "rb").read()
-
-# Analyze the PDF
-results = model.analyze_document(pdf)
-
-# Print the results
-print(results)
+if __name__ == "__main__":
+    main()
